e5ca9753cc94556c908e43b6f342a7be6589b535
Cole <colehartman00@gmail.com>
2024-06-18 15:13:00 -0700

    created homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: [
+    "./*.{html,js}", // Include all HTML and JS files in the root directory
+    "!./*/**",
+  ], // Exclude all files in all subdirectories],
   theme: {
     extend: {},
   },
